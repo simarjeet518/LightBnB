bfb54dd52a4f57d454a6d0b569ada9bc8cd96e22
Simarjeet Kaur <simarjeet518h@gmail.com>
2022-01-06 23:12:21 +0000

    seach filters added

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -104,13 +104,56 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
  const getAllProperties = (options, limit = 10) => {
-  return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => result.rows)
-    .catch((err) => {
-      console.log(err.message);
-    });
-};
+   const queryParams = [];
+   let queryString = `
+   SELECT properties.*, avg(property_reviews.rating) as average_rating
+   FROM properties
+   JOIN property_reviews ON properties.id = property_id
+   `;
+ 
+    
+   if (options.city) {
+     queryParams.push('%'+options.city+'%');
+     queryString += `WHERE city LIKE $${queryParams.length} `;
+   }
+
+   if (options.minimum_price_per_night) {
+    const price = Number(options.minimum_price_per_night );
+    queryParams.push(price*100);
+    queryString += `AND cost_per_night >= $${queryParams.length} `;
+  }
+ 
+  if (options.maximum_price_per_night) {
+    const price = Number(options.maximum_price_per_night );
+    queryParams.push(price*100);
+    queryString += `AND cost_per_night <= $${queryParams.length} `;
+  }
+
+   queryString += `
+   GROUP BY properties.id
+   `;
+
+  if (options.minimum_rating) {
+    const rating = Number(options.minimum_rating);
+    queryParams.push(rating);
+    queryString += `HAVING avg(property_reviews.rating) >= $${queryParams.length} `;
+  }
+
+   queryParams.push(limit);
+   queryString += `
+   ORDER BY cost_per_night 
+   LIMIT $${queryParams.length};
+   `;
+  
+   //console.log(queryString, queryParams);
+ 
+   return pool.query(queryString, queryParams)
+   .then((res) => res.rows)
+   .catch((err) => {
+    console.log(err.message);
+  });
+ };
+
 exports.getAllProperties = getAllProperties;
 
 
